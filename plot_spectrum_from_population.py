16dcaef2f6038def4424b974b6a42e57ba1644b8
kentskind <kentskind@gmail.com>
2017-07-05 19:13:50 +0900

    Change using method from np.loadtxt to pd.read_csv.It makes be faster than previous.

--- plot_spectrum_from_population.py
+++ plot_spectrum_from_population.py
@@ -1,9 +1,10 @@
 import numpy as np
 import json
 import sys
+import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-from Calc_Populations_funcs import *
+from Calc_Populations_funcs_ECCSjson import *
 
 
 def plot_spectrum_from_population():
@@ -19,15 +20,15 @@ def plot_spectrum_from_population():
     print(Collision_system)
     if Collision_system[:3] in Hlike:
         ACfile = open('./ACtoPlotSpectrum/ChiantiAC_{0}_Hlike.json'.format(Collision_system[0]))
-        orbitfile = open('./orbits_Hlike.txt')
+        orbitfile = open('./AtomicOrbitDatasets/orbits_Hlike.txt')
         Energyfile = open('./EnergyLevels/EnergyLevel_{0}_Hlike.json'.format(Collision_system[0]))
     elif Collision_system[:3] in Helike:
         ACfile = open('./ACtoPlotSpectrum/ChiantiAC_{0}_Helike.json'.format(Collision_system[0]))
-        orbitfile = open('./orbits_Helike.txt')
+        orbitfile = open('./AtomicOrbitDatasets/orbits_Helike.txt')
         Energyfile = open('./EnergyLevels/EnergyLevel_{0}_Helike.json'.format(Collision_system[0]))
     elif Collision_system[:3] in Lilike:
         ACfile = open('./ACtoPlotSpectrum/ChiantiAC_{0}_Lilike.json'.format(Collision_system[0]))
-        orbitfile = open('./orbits_Lilike.txt')
+        orbitfile = open('./AtomicOrbitDatasets/orbits_Lilike.txt')
         Energyfile = open('./EnergyLevels/EnergyLevel_{0}_Lilike.json'.format(Collision_system[0]))
     else:
         print('undefined ion type.')
@@ -35,7 +36,9 @@ def plot_spectrum_from_population():
     ACdict = json.load(ACfile)
     orbits = [i.rstrip('\n') for i in orbitfile.readlines()]
     Energydict = json.load(Energyfile)
-    data = np.loadtxt(datafilename,delimiter=',')
+    #data = np.loadtxt(datafilename,delimiter=',')
+    data = pd.read_csv(datafilename,skiprows=1,header=None)
+    data = data.values
 
     print('スペクトルをプロットするポイントを指定してください．({0} - {1})'.format(data[0][0],data[-1][0]))
     plotpoint = float(input())
@@ -58,11 +61,27 @@ def plot_spectrum_from_population():
     
     spectrum_list = []
 
-    for key, val in popdict.items():
-        if key in ACdict:
-            for fin, AC in ACdict[key].items():
-                #print('{0} to {1} : val*BR = {2}, Energy = {3}'.format(key,fin,(val*BR),(Energydict[key]-Energydict[fin])))
-                spectrum_list.append([(val*AC*dt),(Energydict[key]-Energydict[fin])])
+    xaxisflag = True
+    while xaxisflag:
+        xaxisoption = input('横軸を選んでください．(Energy[e] or Wavelength[w]) > ')
+        if xaxisoption == 'e' or xaxisoption == 'w':
+            xaxisflag = False
+        else:
+            print('正しい値を入力してください．')
+
+    if xaxisoption == 'e':
+        for key, val in popdict.items():
+            if key in ACdict:
+                for fin, AC in ACdict[key].items():
+                    #print('{0} to {1} : val*BR = {2}, Energy = {3}'.format(key,fin,(val*BR),(Energydict[key]-Energydict[fin])))
+                    spectrum_list.append([(val*AC*dt),(Energydict[key]-Energydict[fin])])
+    elif xaxisoption == 'w':
+        for key, val in popdict.items():
+            if key in ACdict:
+                for fin, AC in ACdict[key].items():
+                    #print('{0} to {1} : val*BR = {2}, Energy = {3}'.format(key,fin,(val*BR),(Energydict[key]-Energydict[fin])))
+                    spectrum_list.append([(val*AC*dt),1240.0/(Energydict[key]-Energydict[fin])])
+
 
     #print(spectrum_list)
     spectrum = np.array(spectrum_list)
@@ -76,7 +95,10 @@ def plot_spectrum_from_population():
     fig = plt.figure(figsize=(16,9))
     esp = fig.add_subplot(1,1,1)
     esp.stem(spectrum[:,1],spectrum[:,0])
-    esp.set_xlabel('Energy[eV]', fontsize=30)
+    if xaxisoption == 'e':
+        esp.set_xlabel('Energy[eV]', fontsize=30)
+    elif xaxisoption == 'w':
+        esp.set_xlabel('Wavelength[nm]', fontsize=30)
     esp.set_ylabel('Intensity', fontsize=27)
     esp.tick_params(labelleft='off')
     for tick in esp.xaxis.get_major_ticks():
