e2cdcce721dbdcaf5798ea9e0494377b7bd9a9b2
kentskind <kentskind@gmail.com>
2017-10-18 15:56:33 +0900

    PopGraphクラスを使うように変更

--- plotfromdatafile.py
+++ plotfromdatafile.py
@@ -1,6 +1,7 @@
 import numpy as np
 from Calc_Populations_funcs_ECCSjson import *
 import matplotlib.pyplot as plt
+import pandas as pd
 
 
 def plotfromdatafile():
@@ -28,36 +29,23 @@ def plotfromdatafile():
     elif Collision_system[:3] in Li_like:
         iontype = 'Li'
     
-    # 取得されたイオン種から準位名(グラフの凡例に使用)を取得
-    orbitfile = open('./AtomicOrbitDatasets/orbits_{0}like.txt'.format(iontype))
-    orbits = [i.rstrip('\n') for i in orbitfile.readlines()]
-
     # 選択されたデータファイルを読み込む
-    dataarray = np.loadtxt(datafile, delimiter=',')
+    #dataarray = np.loadtxt(datafile, delimiter=',')
+    data = pd.read_csv(datafile,skiprows=1,header=None)
+    dataarray = data.values
 
-    print(dataarray)
-
-    # データの存在する準位の数を取得
-    orbit_nums = len(dataarray[0][2:])
-
-    # グラフプロット処理
-    fig = PopGraph(iontype)
+    graph = PopGraph(iontype)
 
     print('[0] 全ての軌道についての曲線をプロットする\n[1] 指定した軌道のみプロットする')
     plotoption = int(input())
 
     if plotoption == 0:
-        fig.plot(dataarray[:,0], dataarray[:,1:])
-
+        graph.plot(dataarray[:,0], dataarray[:,1:])
     elif plotoption == 1:
-        # データの存在する軌道一覧を表示する
-        fig.selectionPlot(dataarray[:,0],dataarray[:,1:])
-
+        graph.selectionPlot(dataarray[:,0], dataarray[:,1:])
     
-    # プロット方式の設定 
-    fig.setGraph()
-
-    fig.show()
+    graph.setGraph()
+    graph.show()
 
 if __name__ == '__main__':
     plotfromdatafile()
